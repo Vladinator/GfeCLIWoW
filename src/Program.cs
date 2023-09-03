69943fece1ad2041dd0269deb7f6a8b636729671
Vladinator <vladix@gmail.com>
2023-09-03 15:57:01 +0200

    Added offset and using the offset when clipping something from the past, like with CLEU the log gets flushed and there might be a gap, so this offset is nice so we clip the part we care about, without having a lot of padding on the front due to that flush delay.
    Only one export at a time, so the actions queue and use a shared thread safe lock, so anything scheduled just keeps trying until it has a chance to start the GfeCLI itself.

--- src/Program.cs
+++ src/Program.cs
@@ -6,14 +6,20 @@ namespace GfeCLIWoW
     {
         private static readonly Env env = new();
 
-        private static void RunGfeCLI(double duration)
+        private static string DurationToText(double duration)
         {
             TimeSpan timeSpan = TimeSpan.FromMilliseconds(duration);
-            string timeText = string.Format("{0:D2}:{1:D2}:{2:D2}", timeSpan.Hours, timeSpan.Minutes, timeSpan.Seconds);
+            return string.Format("{0:D2}:{1:D2}:{2:D2}", timeSpan.Hours, timeSpan.Minutes, timeSpan.Seconds);
+        }
+
+        private static void RunGfeCLI(double duration, double offset)
+        {
+            string timeText = DurationToText(duration);
+            string offsetText = DurationToText(offset);
             ProcessStartInfo psi = new()
             {
                 FileName = env.GfeCLI,
-                Arguments = $"--process {env.Process} --highlight {timeText}",
+                Arguments = $"--process {env.Process} --highlight {timeText} --offset {offsetText}",
                 WorkingDirectory = Path.GetDirectoryName(env.GfeCLI),
                 UseShellExecute = false,
                 RedirectStandardOutput = true,
@@ -23,20 +29,24 @@ namespace GfeCLIWoW
             {
                 StartInfo = psi
             };
-            process.Start();
-            string error = process.StandardError.ReadToEnd();
-            string output = process.StandardOutput.ReadToEnd();
-            string buffer = $"{error}\n{output}";
-            process.WaitForExit();
-            if (process.ExitCode != 0 || buffer.Contains("unable to save highlight"))
+            try
             {
-                Console.WriteLine($"Error! Unable to save highlight: {timeText}");
+                process.Start();
+                string error = process.StandardError.ReadToEnd();
+                string output = process.StandardOutput.ReadToEnd();
+                process.WaitForExit();
+                string clean = output.Trim();
+                string last = clean.Split("\n").Last();
+                Console.WriteLine($"[{process.ExitCode}] {last ?? clean}");
             }
-            else
+            catch
             {
-                Console.WriteLine($"Done! Saved highlight: {timeText}");
             }
         }
+
+        private static readonly double MAX_HIGHLIGHT_DURATION = 1200000;
+        private static bool IsRunningAction = false;
+        private static readonly object lockObject = new();
 
         private static readonly ManualResetEventSlim exitEvent = new(false);
 
@@ -96,27 +106,63 @@ namespace GfeCLIWoW
             {
                 return;
             }
-            var delta = DateTime.Now - e.Timestamp;
-            var padding = TimeSpan.FromMilliseconds(env.DurationPadding) - delta;
-            var duration = encounterInfo.FightTime;
-            if (padding.TotalMilliseconds > 0)
+            var action = () =>
             {
-                duration += padding.TotalMilliseconds;
-            }
-            else
-            {
-                duration -= padding.TotalMilliseconds;
-                padding = TimeSpan.Zero;
-            }
-            _ = Task.Run(() =>
-            {
-                Console.WriteLine($"Encounter {encounterInfo.ID} \"{encounterInfo.Name}\" on {encounterInfo.Difficulty} ended at {e.Timestamp:HH:mm:ss} after {encounterInfo.FightTime / 1000:0} seconds, clipping {(encounterInfo.Success ? "victory" : "wipe")}{(padding.TotalMilliseconds > 0 ? $" in {padding.TotalSeconds}" : "")}...");
+                var delta = DateTime.Now - e.Timestamp;
+                var offset = delta.TotalMilliseconds;
+                var padding = TimeSpan.FromMilliseconds(env.DurationPadding) - delta;
+                var duration = encounterInfo.FightTime;
                 if (padding.TotalMilliseconds > 0)
                 {
-                    Thread.Sleep(padding);
+                    duration += padding.TotalMilliseconds;
                 }
-                RunGfeCLI(duration + env.DurationPadding);
-            });
+                else
+                {
+                    duration -= padding.TotalMilliseconds;
+                    padding = TimeSpan.Zero;
+                }
+                var totalDuration = duration + env.DurationPadding;
+                var totalDurationAndOffset = totalDuration + offset;
+                if (totalDurationAndOffset > MAX_HIGHLIGHT_DURATION)
+                {
+                    Console.WriteLine($"Unable to save highlight that happened {DurationToText(totalDurationAndOffset)} ago because recording limit is at {DurationToText(MAX_HIGHLIGHT_DURATION)}.");
+                    return;
+                }
+                if (totalDuration > MAX_HIGHLIGHT_DURATION)
+                {
+                    Console.WriteLine($"Unable to save highlight of {DurationToText(totalDuration)} length because recording limit is at {DurationToText(MAX_HIGHLIGHT_DURATION)}.");
+                    return;
+                }
+                Task.Run(() =>
+                {
+                    Console.WriteLine($"Encounter {encounterInfo.ID} \"{encounterInfo.Name}\" on {encounterInfo.Difficulty} ended at {e.Timestamp:HH:mm:ss} after {encounterInfo.FightTime / 1000:0} seconds - clipping {(encounterInfo.Success ? "victory" : "wipe")}{(padding.TotalMilliseconds > 0 ? $" in {padding.TotalSeconds}" : "")}...");
+                    if (padding.TotalMilliseconds > 0)
+                    {
+                        Thread.Sleep(padding);
+                    }
+                    RunGfeCLI(totalDuration, offset);
+                }).Wait();
+            };
+            var queuedAction = () =>
+            {
+                lock (lockObject)
+                {
+                    while (IsRunningAction)
+                    {
+                        Thread.Sleep(100);
+                    }
+                    IsRunningAction = true;
+                }
+                try
+                {
+                    action();
+                }
+                catch
+                {
+                }
+                IsRunningAction = false;
+            };
+            _ = Task.Run(queuedAction);
         }
 
         private void ReaderLogChanged(object? sender, LogReaderEventArgs e)
